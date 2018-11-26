b98f0260a448efc682831220920b756e33af02cf
Grom Olga <GromOlga.work@gmail.com>
2018-11-26 14:37:41 +0200

    created empty theme

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -41,6 +41,16 @@ module.exports = {
         ],
         dsl: 'less'
     },
+    default: {
+        area: 'frontend',
+        name: 'Project/Default',
+        locale: 'he_IL',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     backend: {
         area: 'adminhtml',
         name: 'Magento/backend',
