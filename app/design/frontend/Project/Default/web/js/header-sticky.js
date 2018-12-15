eaa6f25cf756c77e9f8bbe5b525c01ebf8a19485
Grom Olga <gromolga.work@gmail.com>
2018-12-15 15:12:45 +0200

    completed footer, fixed header sticky

--- app/design/frontend/Project/Default/web/js/header-sticky.js
+++ app/design/frontend/Project/Default/web/js/header-sticky.js
@@ -1,14 +1,19 @@
 define(['jquery'], function ($) {
     return function (config, element) {
         var $element = $(element);
+        var $header = $('[data-action="header-sticky"]');
+        var headerHeight = $header.outerHeight();
         var onScroll = function(){
-            if($(window).scrollTop() > 50){
+            if($(window).scrollTop() > 120){
                 $element.addClass("header-sticky");
+                $element.next().height(headerHeight);
             } else {
                 $element.removeClass("header-sticky");
+                $element.next().height(0);
             }
         };
         $(window).scroll(onScroll);
         onScroll();
     };
 });
+
